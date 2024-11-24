4437f332292ef14683aa8fd02ffb24d888ddecfc
suraj <surajrace21@gmail.com>
2024-11-24 23:40:21 +0530

    hero-bg-made-for-big-screen

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import "./App.css"
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+      App</div>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Css/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome to My Page</h1>
+      <div className="sphere"></div>
+      <div className="star1"></div>
+      <div className="star2"></div>
+      <div className="star3"></div>
+
+
+
+    </div>
+  );
+};
+
+export default Hero;
